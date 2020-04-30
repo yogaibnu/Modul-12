019b0fb79468a4c609c2de88fae715da9fe7924f
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-30 10:57:33 +0700

    edit Post.jsx

--- src/component/BlogPost/Post.jsx
+++ src/component/BlogPost/Post.jsx
@@ -9,7 +9,10 @@ const Post = (props) => {
             <div className="konten-artikel">
                 <div className="judul-artikel">{props.judul}</div>
                 <p className="isi-artikel">{props.isi}</p>
-                <button className="btn btn-sm btn-warning" onClick={() => props.hapusArtikel(props.idArtikel)}>Hapus</button>
+                <button className="btn btn-sm btn-warning" 
+                    onClick={() => { if (window.confirm('Apakah anda yakin menghapus artikel ini?')) props.hapusArtikel(props.idArtikel)}}>
+                    Hapus
+                </button>
             </div>
         </div>
     )
