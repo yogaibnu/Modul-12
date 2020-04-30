fed14ddb182c9719c266e15c2e03b3267e2a574f
yogaibnu <yogaibnudinataa@gmail.com>
2020-04-30 10:16:41 +0700

    create firebase/config.js di src

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUm6k-TmJL2PrdEgzsKB3QdpSk5PvVHOU",
+    authDomain: "fir-login-7aa67.firebaseapp.com",
+    databaseURL: "https://fir-login-7aa67.firebaseio.com",
+    projectId: "fir-login-7aa67",
+    storageBucket: "fir-login-7aa67.appspot.com",
+    messagingSenderId: "1034079241165",
+    appId: "1:1034079241165:web:f74dc01e08f82209497a84"
+  };
+
+  export default firebaseConfig;
